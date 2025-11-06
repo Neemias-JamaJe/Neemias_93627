1a14429cdc632ed4ef088c31cbbbaeb9a6c6c4bf
Neemias-JamaJe <neemiasvidalmedeiros@gmail.com>
2025-11-06 11:19:28 -0300

    Add files via upload

---
+++ FLASK/app.py
@@ -0,0 +1,19 @@
+
+
+#INCLUINDO A BIBLIOTECA
+from flask import Flask, render_template, jsonify
+app = Flask(__name__)
+
+# Página principal
+@app.route('/')
+def home():return render_template('index.html')
+
+@app.route('/projetos')
+def projetos():return render_template('projeto.html')
+
+# API simples
+@app.route('/api/mensagem')
+def mensagem():return jsonify({'mensagem': 'Olá, mundo!'})
+
+# Iniciar o site
+if __name__ == '__main__':app.run(debug=True)
